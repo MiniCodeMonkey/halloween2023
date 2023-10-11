df19b47fb66d15a1340cbe368844ddb95bb1787a
Mathias Hansen <me@codemonkey.io>
2023-10-11 21:21:17 +0200

    fix(tripwire): Clean up

--- tripwire/tripwire/tripwire.ino
+++ tripwire/tripwire/tripwire.ino
@@ -37,11 +37,9 @@ void populateChipId() {
 
 void initMQTT()
 {
-  // Optional functionalities of EspMQTTClient
-  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
-  client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
-  client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
-  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  client.enableHTTPWebUpdater();
+  client.enableOTA();
+  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");
 }
 
 // This function is called once everything is connected (Wifi and MQTT)
